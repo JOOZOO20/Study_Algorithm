41e0bd13a37170d3fdec0c43c8c2f9c5ef255a9a
JOOZOO20 <qwsa7896@naver.com>
2020-09-20 17:22:55 +0900

    백준 알고리즘 9498번
    백준 알고리즘 9498번 - if문 사용 문제

---
+++ 9498번.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf_s("%d", &a);
+
+	if (a >= 90 && a <= 100)
+		printf("A\n");
+	else if (a >= 80 && a < 90)
+		printf("B\n");
+	else if (a >= 70 && a < 80)
+		printf("C\n");
+	else if (a >= 60 && a < 70)
+		printf("D\n");
+	else
+		printf("F\n");
+
+	return 0;
+	
+}
